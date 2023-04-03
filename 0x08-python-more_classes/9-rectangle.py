81a35fdff06d4cb9711881677f12ced01fb89ee5
Joseph Otine <joeotine@gmail.com>
2023-04-03 11:25:40 -0700

    Python - More Classes and Objects tasks

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -82,4 +82,5 @@ class Rectangle:
     @classmethod
     def square(cls, size=0):
         """Create a new Rectangle instance with width == height == size."""
-        return cls(size, size)
+        return cls(size, size
+                )
