2c4cce929dd1e2f945fe2f5c1140a1c92b1b94a4
Joseph Otine <joeotine@gmail.com>
2023-04-11 06:01:58 -0700

    Python - Inheritance tasks

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Defines an empty class BaseGeometry."""
+
+class BaseGeometry:
+    """Represents a base geometry."""
+    pass
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a base geometry class BaseGeometry."""
+
+class BaseGeometry:
+    """Represents a base geometry."""
+
+    def area(self):
+        """Not implemented."""
+        raise Exception("area() is not implemented")
