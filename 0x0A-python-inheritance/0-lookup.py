458b1a7e364d7a2d2952a07608e2f8fcf955f353
Joseph Otine <joeotine@gmail.com>
2023-04-11 04:28:12 -0700

    Python - Inheritance tasks

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-"""This defines an object attribute function."""
+"""It defines an object attribute function."""
 
 def lookup(obj):
-    """This returns a list of an objects available attributes."""
+    """It returns a list of an objects available attributes."""
     return (dir(obj))
 
