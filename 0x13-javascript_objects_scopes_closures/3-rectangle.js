03396fbb78cc1724cd131b7629ee8a30c32edd63
Joseph Otine <joeotine@gmail.com>
2023-06-13 11:59:51 -0700

    JavaScript - Objects, Scopes and Closures all tasks done

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) return;
-    this.width = w;
-    this.height = h;
+    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
   }
 
   print () {
     for (let i = 0; i < this.height; i++) {
-	    console.log('X'.repeat(this.width));
+      console.log('X'.repeat(this.width));
     }
   }
 };
--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,24 +1,20 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) return;
-    this.width = w;
-    this.height = h;
+    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
   }
 
-  print (char = 'X') {
+  print () {
     for (let i = 0; i < this.height; i++) {
-	    console.log(char.repeat(this.width));
+      console.log('X'.repeat(this.width));
     }
   }
 
   rotate () {
-    [this.height, this.width] = [this.width, this.height];
+    [this.width, this.height] = [this.height, this.width];
   }
 
   double () {
-    this.height = this.height * 2;
-
-    this.width = this.width * 2;
+    [this.width, this.height] = [this.width * 2, this.height * 2];
   }
 };
